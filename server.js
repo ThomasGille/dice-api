28d2afb29c8fec95d920fe130ce91b938dfdf83d
Thomas GILLE <totogille@gmail.com>
2018-02-18 19:58:49 +0100

    first commit to prod

---
+++ server.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const Cat = mongoose.model('Cat', { name: String });
+
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+let dbURI = process.env.MONGODB_URI;
+if(!dbURI) {
+    dbURI = 'mongodb://localhost/test';
+}
+mongoose.connect(dbURI);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        Cat.find(function (err, cats) {
+            if (err) return console.error(err);
+            reply(cats);
+        });
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/users/{name}',
+    handler: function (request, reply) {
+        const kitty = new Cat({ name: request.params.name });
+        kitty.save().then((kitty) => reply(kitty)).catch((e) => {console.log(e)});
+    },
+    config: {
+        validate: {
+            params: {
+                name: Joi.string().min(3).max(20)
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'DELETE',
+    path: '/users/{name}',
+    handler: function (request, reply) {
+        Cat.find({
+            name: request.params.name
+        }, function (err, docs) {
+            docs.forEach((el) => el.remove()); //Remove all the documents that match!
+            reply('OK, DELETED!');
+        });
+    },
+    config: {
+        validate: {
+            params: {
+                name: Joi.string().min(3).max(20)
+            }
+        }
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
